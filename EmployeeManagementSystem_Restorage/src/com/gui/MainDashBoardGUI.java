cfeaf4c2c5b3fc1ff9458be4610bd685db7939d7
DELL <DELL@10.0.0.38>
2017-11-21 13:51:54 -0500

    MainDashboard

--- EmployeeManagementSystem_Restorage/src/com/gui/MainDashBoardGUI.java
+++ EmployeeManagementSystem_Restorage/src/com/gui/MainDashBoardGUI.java
@@ -1,21 +1,45 @@
 package com.gui;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
 public class MainDashBoardGUI {
 
 	JFrame mainDashBoardFrame;
-	Menu menu;  
+	JMenu menu;  
     JMenuItem register, admin;
-	public MainDashBoardGUI() {
+	public MainDashBoardGUI() 
+	{
 		// TODO Auto-generated constructor stub
-	
-	
+		 JFrame f= new JFrame("Employee Management System");  
+         JMenuBar mb=new JMenuBar();  
+         menu=new JMenu("Menu");  
+         admin=new JMenuItem("Admin");  
+         register=new JMenuItem("Register");  
+         menu.add(admin); menu.add(register);  
+         mb.add(menu);  
+         f.setJMenuBar(mb);  
+         f.setSize(400,400);  
+         f.setLayout(null);  
+         f.setVisible(true);  
+         admin.addActionListener(new ActionListener(){
+             public void actionPerformed(ActionEvent a) {
+                   // calling method resetFields()
+                   new AdminLoginGUI();
+             }
+         });
+         register.addActionListener(new ActionListener(){
+             public void actionPerformed(ActionEvent a) {
+                   // calling method resetFields()
+                   new RegisterGUI();
+             }
+         });
 	}
+	
 	public static void main(String args[]){
-		
+	 new MainDashBoardGUI();	
 	}
 
 }
--- EmployeeManagementSystem_Restorage/src/com/gui/RegisterGUI.java
+++ EmployeeManagementSystem_Restorage/src/com/gui/RegisterGUI.java
@@ -1,7 +1,25 @@
 package com.gui;
 
+import java.sql.*;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
 public class RegisterGUI {
 
+	
+	JFrame registerFrame;
+	Connection con;
+    Statement stmt;
+    PreparedStatement preStatement;
+    JLabel firstNameLabel, lastNameLabel,genderLabel,phoneLabel,createPasswordLabel,confirmPasswordLabel;
+    JTextField firstNameTextBox, lastNameTextBox,phoneTextBox,createPasswordTextBox,confirmPasswordTextBox;
+    JButton registerButton, exitButton,updateButton,deleteButton,resetButton,refresh;
+    JRadioButton male, female;
+    ButtonGroup bg;
+    JPanel registerPanel;
 	public RegisterGUI() {
 		// TODO Auto-generated constructor stub
 	}
