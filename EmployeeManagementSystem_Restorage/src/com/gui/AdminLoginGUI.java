2a5a895cab8ba987a9476ee0f688632081f23a36
DELL <DELL@10.0.0.38>
2017-12-01 00:41:23 -0500

    Admin form validation and authentication complete.

--- EmployeeManagementSystem_Restorage/src/com/gui/AdminLoginGUI.java
+++ EmployeeManagementSystem_Restorage/src/com/gui/AdminLoginGUI.java
@@ -1,15 +1,159 @@
 package com.gui;
 
-import javax.swing.JFrame;
+
+import java.awt.Font;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+import javax.swing.*;
+
+
 
 public class AdminLoginGUI {
 
-	public AdminLoginGUI() {
-		// TODO Auto-generated constructor stub
-				JFrame adminFrame= new JFrame("Admin Login");  
-//				registrationFrame.setSize(400,400);  
-//				registrationFrame.setLayout(null);  
-//				registrationFrame.setVisible(true);
+	
+	JFrame adminLoginFrame;
+	Connection con;
+    Statement stmt;
+    PreparedStatement preStatement;
+    ResultSet res;
+    JLabel userNameLabel,passwordNameLabel, banner;
+    JTextField userNameTextBox;
+    JPasswordField passwordTextBox;
+    JButton loginButton;
+    JPanel adminPanel;
+    String userName,adminPassword;
+	int count =0;
+    public AdminLoginGUI() 
+	{
+		
+    	adminLoginFrame=new javax.swing.JFrame("Admin Login");
+		adminPanel=new javax.swing.JPanel();
+		adminLoginFrame.add(adminPanel);
+		adminPanel.setLayout(null);
+		adminLoginFrame.setSize(970,700);
+		adminLoginFrame.show();
+		
+	    
+		banner=new javax.swing.JLabel("Admin Login");
+		banner.setFont(new Font("Dialog", Font.ITALIC, 24));
+		banner.setBounds(300,20,400,40);
+		adminPanel.add(banner);
+	    
+		userNameLabel=new javax.swing.JLabel("User name");
+		userNameLabel.setFont(new Font("Dialog", Font.BOLD, 20));
+		userNameLabel.setBounds(100,80,150,40);
+		adminPanel.add(userNameLabel);
+		userNameTextBox=new javax.swing.JTextField();
+		userNameTextBox.setFont(new Font("Dialog", Font.BOLD, 16));
+		userNameTextBox.setBounds(350,80,180,30);
+		adminPanel.add(userNameTextBox);
+	      
+		passwordNameLabel = new JLabel("Password"); 
+		passwordNameLabel.setFont(new Font("Dialog", Font.BOLD, 20));
+		passwordNameLabel.setBounds(100, 140, 150, 40);
+		adminPanel.add(passwordNameLabel);
+		passwordTextBox = new JPasswordField(); 
+		passwordTextBox.setFont(new Font("Dialog", Font.BOLD, 16));
+		passwordTextBox.setBounds(350, 140, 180, 30);
+		adminPanel.add(passwordTextBox);
+		
+		
+
+		// Defining Register Button
+		loginButton = new JButton("Register");
+		loginButton.setFont(new Font("Dialog", Font.BOLD, 16));
+		loginButton.setBounds(100,200,100,30);
+		adminPanel.add(loginButton);
+		loginButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event)
+			{
+
+			getUserInformation();
+			validation();
+			try {
+				registerUserData();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			}
+
+		});
+		
+
+
+		
+
+		
+		adminLoginFrame.add(adminPanel);
+		adminLoginFrame.setLayout(null);  
+		adminLoginFrame.setVisible(true);
+		
+        
+
+        
+		
 	}
+	public void getUserInformation() {
+		userName = userNameTextBox.getText();
+		adminPassword = passwordTextBox.getText();
+		
+	}
+	public boolean validation()
+	{
+		count=0;
+		if(userName.equals(""))
+		{
+			JOptionPane.showMessageDialog(null,"Enter Id");
+			return false;
+		}
+		else if(adminPassword.equals(""))
+		{
+			JOptionPane.showMessageDialog(null,"Enter password");
+			return false;
+		}
+		else 
+		{
+			count=1;
+			return true;
+		}
+	}
+	public void registerUserData() throws SQLException
+	{
+		try
+		{
+			if(count==1)
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeems","root","system");
+				stmt = con.createStatement();
+				String adminLogin="select name,admin_password from admin where name='"+userName+"' and admin_password='"+adminPassword+"'";
+		    	res = stmt.executeQuery(adminLogin);
+		    	if(res.first()){
+		    		new AdminDashBoardGUI();
+		    		JOptionPane.showMessageDialog(null,"Logged in");	
+		    	}else{
+		    		JOptionPane.showMessageDialog(null,"Invalid Id and Password");	
+		    	}
+				
+           	 }
+		}
+		catch (Exception e) 
+		{
+				System.out.println("Exception1 is " + e);
+		}	
+		finally{
+			stmt.close();
+			con.close();
+	
+		}
+	}
+		
+	
 
 }
